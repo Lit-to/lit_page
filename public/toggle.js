d87a447c83a352e80c611f6c867062703ac43089
Lit-to <swordworld0327@gmail.com>
2024-08-14 09:45:01 +0900

    仮で見た目編集はここまで もうhtmlは嫌いだ

--- public/toggle.js
+++ public/toggle.js
@@ -1,3 +1,4 @@
+//ダークモード/ライトモード
 const toggle = document.getElementById('theme-toggle');
 toggle.addEventListener('change', function() {
     if (this.checked) {
@@ -6,3 +7,13 @@ toggle.addEventListener('change', function() {
         document.body.classList.remove('dark-mode');
     }
 });
+//ハンバーガーメニュー
+document.querySelector('.hamburger-icon').addEventListener('click', function() {
+    const sidebar = document.querySelector('.sidebar');
+    if (sidebar.style.left === '0%') {
+        sidebar.style.left = '-15%';
+    } else {
+        sidebar.style.left = '0%';
+    }
+});
+
